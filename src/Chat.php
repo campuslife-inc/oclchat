30d335b627fd47b22ed9ffc3fe28ae323913a32d
surekha202 <surekharekha14@gmail.com>
2024-09-16 11:39:32 +0530

    chat message chages

--- src/Chat.php
+++ src/Chat.php
@@ -76,23 +76,25 @@ class Chat implements MessageComponentInterface {
 
             // Check if either replyMessageId or chatReplayId is present and not equal to 0
             if (isset($data['replyMessageId']) || isset($data['chatReplayId'])) {
-
-                $private_chat_object->setReplyMessageId($data['replyMessageId']);
-                $private_chat_object->setParentReplyMessageId($data['chatReplayId']);
-                $reply_to = $private_chat_object->getReplyToMessage($data['chatReplayId']);
-            }
-            else if(isset($data['replyMessageId']))
-            {
-                $private_chat_object->setReplyMessageId($data['replyMessageId']);
-                $private_chat_object->setParentReplyMessageId(null);
-                $reply_to = $private_chat_object->getReplyToMessage($data['replyMessageId']);
-            }
-            else
-            {
-                $private_chat_object->setReplyMessageId(null);
-                $private_chat_object->setParentReplyMessageId(null);
-                $reply_to = null;
-            }
+                        echo "hai replytoreply";
+                        $private_chat_object->setReplyMessageId($data['chatReplayId']);
+                        $private_chat_object->setParentReplyMessageId($data['replyMessageId']);
+                        $reply_to = $private_chat_object->getReplyToMessage($data['chatReplayId']);
+                    }
+                    else if(isset($data['replyMessageId']))
+                    {
+                        echo "hai replyyyyyyyy";
+                        $private_chat_object->setReplyMessageId($data['replyMessageId']);
+                        $private_chat_object->setParentReplyMessageId(null);
+                        $reply_to = $private_chat_object->getReplyToMessage($data['replyMessageId']);
+                    }
+                    else
+                    {
+                        $private_chat_object->setReplyMessageId(null);
+                        $private_chat_object->setParentReplyMessageId(null);
+                        $reply_to = null;
+                    }
+        
 
                         
 			$chat_message_id = $private_chat_object->save_chat();
